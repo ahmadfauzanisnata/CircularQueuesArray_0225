e96acb0989254b2d880ffc02db4ab7d1e865e1c5
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-30 15:35:13 +0700

    membuat fungsi main untuk menjalankan sistem yang telah dibuat dan memberi nama alias bagi Queues dan char

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -14,31 +14,25 @@ class Queues {
     }
 
     void insert() {
-        int num;
-        cout << "Enter a number: ";
-        cin >> num;
-        cout << endl;
-
-
-        // cek apakah antrian penuh
-        if ((FRONT == 0 && REAR == max -1)) || ((FRONT == REAR +1)) {
-        cout << "\nQueue overflow\n";
-        return;
+               // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
         }
 
-         // cek apakah antrian kosong
+        // cek apakah antrian kosong
         if (FRONT == -1) {
-        FRONT = 0;
-        REAR = 0;
+            FRONT = 0;
+            REAR = 0;
+        } else {
+            // jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
         }
-         else {
-         // jika REAR berada di posisi terakhir array, kembali ke awal array
-        if (REAR == max -1)
-        REAR = 0;
-        else
-        REAR = REAR + 1;
-         }
-         queue_array[REAR] = num;
+        queue_array[REAR] = num;
+
 
     } 
 
@@ -92,3 +86,9 @@ class Queues {
     
 
 };
+
+int main() {
+    Queues q;
+    char ch;
+    
+}
